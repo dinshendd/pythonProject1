f49798974dd6f6380e693617608337c5e0a07366
Sheng <sding1@seas.upenn.edu>
2023-02-11 21:50:48 -0500

    a

--- ipfs.py
+++ ipfs.py
@@ -8,7 +8,11 @@ def pin_to_ipfs(data):
     projectSecret = "1cc5f367c0b20a043728edeb9a8a280f"
     endpoint = "https://ipfs.infura.io:5001"
 
-    response = requests.post(endpoint + '/api/v0/add', files=data, auth=(projectID, projectSecret))
+    files = {
+        'file': data
+    }
+
+    response = requests.post(endpoint + '/api/v0/add', files=files, auth=(projectID, projectSecret))
     print(response.text)
     cid = response.text.split(",")[1].split(":")[1].replace('"', '')
     return cid
