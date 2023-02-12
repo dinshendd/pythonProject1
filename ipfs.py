b1f5bbe679158b454546c1fa567111f915aba127
Sheng <sding1@seas.upenn.edu>
2023-02-11 21:47:05 -0500

    a

--- ipfs.py
+++ ipfs.py
@@ -14,4 +14,9 @@ def pin_to_ipfs(data):
     return cid
 
 
-pin_to_ipfs("hello world")
+def get_from_ipfs(cid,content_type="json"):
+	assert isinstance(cid,str), f"get_from_ipfs accepts a cid in the form of a string"
+	#YOUR CODE HERE
+
+	assert isinstance(data,dict), f"get_from_ipfs should return a dict"
+	return data
